89b302ac8c8412f73775580058e4e763d3d59994
Vinicius Barcelos <josevsaraiva@gmail.com>
2020-11-27 17:29:09 -0300

    docs: install jdoc and documents methods

--- src/repositories/UsersRepository.js
+++ src/repositories/UsersRepository.js
@@ -1,6 +1,33 @@
 const { User } = require('../models');
 
+/**
+ * @typedef { Object } UserDTO
+ * @property { string } name
+ * @property { string } email
+ * @property { string } password
+ * @property { string } type
+ *
+ */
+
+/**
+ * @typedef { Object } User
+ * @property { string } id
+ * @property { string } name
+ * @property { string } email
+ * @property { string } password
+ * @property { string } type
+ * @property { Date } createdAt
+ * @property { Date } updatedAt
+ */
+/**
+ * @class UsersRepository
+ */
 class UsersRepository {
+  /**
+   *
+   * @param { UserDTO } data
+   * @returns { Promise<User> }
+   */
   async create({ name, email, password, type }) {
     const user = User.build({
       name,
@@ -14,6 +41,11 @@ class UsersRepository {
     return user;
   }
 
+  /**
+   *
+   * @param { string } email
+   * @return { Promise<User> }
+   */
   async findByEmail(email) {
     const user = await User.findOne({
       where: {
--- src/services/CreateUserService.js
+++ src/services/CreateUserService.js
@@ -1,11 +1,40 @@
 const { hash } = require('bcryptjs');
 const UsersRepository = require('../repositories/UsersRepository');
 
+/**
+ * @typedef { Object } Request
+ * @property { string } name
+ * @property { string } email
+ * @property { string } password
+ * @property { string } type
+ *
+ */
+
+/**
+ * @typedef { Object } User
+ * @property { string } id
+ * @property { string } name
+ * @property { string } email
+ * @property { string } password
+ * @property { string } type
+ * @property { Date } createdAt
+ * @property { Date } updatedAt
+ */
+
+/**
+ * Instance a CreateUserService
+ * @constructor
+ */
 class CreateUserService {
   constructor() {
     this.usersRepository = new UsersRepository();
   }
 
+  /**
+   *
+   * @param { Request } data
+   * @returns { Promise<User> }
+   */
   async execute({ name, email, password, type }) {
     if (type !== 'student' && type !== 'teacher') {
       throw new Error('Invalid user type');
