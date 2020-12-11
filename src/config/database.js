b4488cf07646db5dc829a8cec83c83e130453ba7
Vinicius Barcelos <josevsaraiva@gmail.com>
2020-12-10 22:00:07 -0300

    refactor(database): use the .env config to set up the database informations

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'mich',
-    password: 'admin',
+    username: process.env.USER_DB,
+    password: process.env.PASS_DB,
     database: 'tae_quiz',
-    host: '127.0.0.1',
+    host: process.env.HOST_DB,
     dialect: 'mysql',
   },
   test: {
